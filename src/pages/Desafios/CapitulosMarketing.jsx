415cbad3782e7c051e26e79bb946ffcdc04a0930
Victor Nunes <lima.juliana453@gmail.com>
2026-02-04 11:25:22 -0300

    hgjighkj

--- src/pages/Desafios/CapitulosMarketing.jsx
+++ src/pages/Desafios/CapitulosMarketing.jsx
@@ -13,7 +13,7 @@ function ChallengeList() {
         {/* Desafio 1 */}
         <Link to="/desafios/Marketing/DesafioMar1" className={styles.challengeCard}>
           <img
-            src="https://imgur.com/IBcmitz.jpg" 
+            src="https://media.licdn.com/dms/image/v2/C4D12AQFFL0046WJljw/article-cover_image-shrink_600_2000/article-cover_image-shrink_600_2000/0/1520187871347?e=2147483647&v=beta&t=44dHEfB5m1EUwonP-PdqfRcltFpNDbvhsRlfMZ7z-n0" 
           ></img>
           <p>SEO & Conteúdo</p> 
         </Link>
@@ -29,26 +29,26 @@ function ChallengeList() {
         {/* Desafio 3 */}
         <Link to="/desafios/Marketing/DesafioMar3" className={styles.challengeCard}>
          <img
-            src="https://imgur.com/uAH3O0f.jpg" 
+            src="https://www.nacionalvox.com.br/wp-content/uploads/2022/06/automacao-marketing-digital-1024x597.jpg" 
           ></img>
           <p>Automação</p>
         </Link>
         {/* Desafio 4 */}
         <Link to="/desafios/Marketing/DesafioMar4" className={styles.challengeCard}>
          <img
-            src="https://imgur.com/n4dfJ4f.jpg"
+            src="https://cdn.prod.website-files.com/645c64c06e6e2600a0067ccf/651dd88b3241d469eea73731_metas%20de%20vendas.jpg"
           ></img>
           <p>Análise e Estratégia</p>
         </Link>
         <Link to="/desafios/Marketing/DesafioMar5" className={styles.challengeCard}>
          <img
-            src="https://imgur.com/n4dfJ4f.jpg"
+            src="https://blog.nectarcrm.com.br/content/images/2023/01/Vendas-B2B---estrat-gias-para-vender-mais.png"
           ></img>
           <p>CRM & Ferramentas</p>
         </Link>
         <Link to="/desafios/Tecnologia/DesafioTec4" className={styles.challengeCard}>
          <img
-            src="https://imgur.com/n4dfJ4f.jpg"
+            src="https://www.kawaiikakkoiisugoi.com/wp-content/uploads/2022/10/L1040003-scaled.jpg"
           ></img>
           <p>CRM</p>
         </Link>
