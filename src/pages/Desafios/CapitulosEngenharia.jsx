d1da1fed4ee86292b01ab6af7400ba70284834f7
Matheus Muniz <theumuniz2877@gmail.com>
2026-02-04 10:19:39 -0300

    coloquei o link das imagem de eng

--- src/pages/Desafios/CapitulosEngenharia.jsx
+++ src/pages/Desafios/CapitulosEngenharia.jsx
@@ -14,7 +14,7 @@ function ChallengeList() {
            {/* Desafio 1 */}
                <Link to="/Desafios/Engenharia/DesafioEng1" className={styles.challengeCard}>
                  <img
-                   src="/img_desafios/proj-estrutural.jpg"
+                   src="https://github.com/CarlosDHC/cyber-tech-v.2.0/blob/main/public/img_desafios/proj-estrutural.jpg?raw=true"
                  ></img>
                  <p>Projeto Estrutural</p> 
                </Link>
@@ -22,7 +22,7 @@ function ChallengeList() {
                {/* Desafio 2 */}
                <Link to="/desafios/Engenharia/DesafioEng2" className={styles.challengeCard}>
                  <img
-                   src="/img_desafios/planejamento-urbano.jpg"
+                   src="https://github.com/CarlosDHC/cyber-tech-v.2.0/blob/main/public/img_desafios/planejamento-urbano.jpg?raw=true"
                  ></img>
                  <p>Planejamento Urbano</p> 
                </Link>
@@ -30,38 +30,38 @@ function ChallengeList() {
                {/* Desafio 3 */}
                <Link to="/desafios/Engenharia/DesafioEng3" className={styles.challengeCard}>
                 <img
-                   src="/img_desafios/infra-eng.jpg"
+                   src="https://github.com/CarlosDHC/cyber-tech-v.2.0/blob/main/public/img_desafios/infra-eng.jpg?raw=true"
                  ></img>
                  <p>Infraestrutura</p>
                </Link>
                {/* Desafio 4 */}
                <Link to="/desafios/Engenharia/DesafioEng4" className={styles.challengeCard}>
                 <img
-                   src="/img_desafios/geo-solos.jpg"
+                   src="https://github.com/CarlosDHC/cyber-tech-v.2.0/blob/main/public/img_desafios/geo-solos.jpg?raw=true"
                  ></img>
                  <p>Geotecnia e Solos</p>
                </Link>
                <Link to="/desafios/Engenharia/DesafioEng5" className={styles.challengeCard}>
                 <img
-                   src="/img_desafios/pontes-viadutos.jpg"
+                   src="https://github.com/CarlosDHC/cyber-tech-v.2.0/blob/main/public/img_desafios/pontes-viadutos.jpg?raw=true"
                  ></img>
                  <p>Pontes e Viadutos</p>
                </Link>
                <Link to="/desafios/Engenharia/DesafioEng6" className={styles.challengeCard}>
                 <img
-                   src="/img_desafios/const.jpg"
+                   src="https://github.com/CarlosDHC/cyber-tech-v.2.0/blob/main/public/img_desafios/const.jpg?raw=true"
                  ></img>
                  <p>Constituição</p>
                </Link>
                <Link to="/desafios/Engenharia/DesafioEng7" className={styles.challengeCard}>
                 <img
-                   src="/img_desafios/processo-eng.jpg"
+                   src="https://github.com/CarlosDHC/cyber-tech-v.2.0/blob/main/public/img_desafios/processo-eng.jpg?raw=true"
                  ></img>
                  <p>Processo</p>
                </Link>
                <Link to="/desafios/Engenharia/DesafioEng8" className={styles.challengeCard}>
                 <img
-                   src="/img_desafios/fundacoes.jpg"
+                   src="https://github.com/CarlosDHC/cyber-tech-v.2.0/blob/main/public/img_desafios/fundacoes.jpg?raw=true"
                  ></img>
                  <p>Fundações</p>
                </Link>
